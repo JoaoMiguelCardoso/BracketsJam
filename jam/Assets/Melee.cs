8915c95679456dd2817e6463b3cf802aacd918e4
emersonsamuel1 <emersonsamuel_@hotmail.com>
2021-08-24 22:32:38 -0300

    asd

--- jam/Assets/Melee.cs
+++ jam/Assets/Melee.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Melee : MonoBehaviour
 {
+    public Transform papai;
+    public float radius;
+    public LayerMask balaInimigo;
+
+    //public static int OverlapCircle(papai.position,radius,balaInimigo);
+    Collider2D[] tiro;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class Melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector2 a = Physics2D.OverlapCircle(papai.position,radius,balaInimigo);
+       // foreach(Collider2D[] balas in Physics2D.OverlapCircle(papai.position,radius,balaInimigo))
+        {
+
+        }
     }
 }
